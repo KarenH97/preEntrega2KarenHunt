7abdc5e569ebbbbc13c9397d08bcdc9efe159a42
Karen Hunt <de.and.va.karenh@gmail.com>
2023-09-16 02:43:41 -0300

    simulador con menu terminado

--- js/tienda.js
+++ js/tienda.js
@@ -6,176 +6,120 @@
  let nombre = prompt("Ingrese su nombre")
  alert(`Bienvendo/a ${nombre} a nuestra tienda de joyas, esperamos puedas encontrar alguna joya de tu interés `)
  const IVA = 0.21
- let total = 0
 
  class Tienda{
-     constructor(idI, tipoI, caracI, precioI) {
-        this.id = idI;
+     constructor( tipoI, caracI, precioI) {
         this.tipo = tipoI;
-        this.caracteristicas = caracI;
+        this.carac = caracI;
         this.precio = precioI;
 }
         mostrarProducto(){
-            console.log(`El producto ${this.tipo}, de ${this.caracteristicas}, tiene un precio de $${this.precio}`)
+            console.log(`· ${this.tipo} ${this.carac}, tiene un precio de $${this.precio}`)
         }
         calcuIva = () =>{
            return  this.precio * IVA
             }
         precioMasIva = (resIva) =>{
-           console.log(`El precio del producto ${this.tipo}, en total, con la suma del impuesto iva que es del %${IVA}, da $` + (resIva + this.precio))
+           console.log(`El precio del producto ${this.tipo}, con la suma del impuesto iva, que es del %${IVA}, da  la suma total de $` + (resIva + this.precio))
         }
  }
 
-const producto1 = new Tienda(1, "Collar", " plata con piedra onix", 15000)
-console.log(producto1)
-const producto2 = new Tienda(2,"Aros", "tipo argollas", 6000)
-console.log(producto2)
-const producto3 = new Tienda(3,"Anillo", " plata con piedra rose", 20000)
-console.log(producto3)
-const producto4 = new Tienda(4, "Pulsera", " plata con marcasitas", 9000)
-console.log(producto4)
 
- console.log(producto1.calcuIva())
- producto1.precioMasIva(producto1.calcuIva())
+const producto1 = new Tienda( "Collar", "de plata con piedra onix", 15000)
 
-producto1.mostrarProducto()
-producto2.mostrarProducto()
-producto3.mostrarProducto()
-producto4.mostrarProducto()
+const producto2 = new Tienda("Aros", "de tipo argollas", 6000)
+
+const producto3 = new Tienda("Anillo", "de plata con piedra rose", 20000)
+
+const producto4 = new Tienda( "Pulsera", "de plata con marcasitas", 9000)
+
+
+const catProductos = [producto1, producto2, producto3, producto4]
 
 
 
+ function menu (){
+    let salirMenu = false
+
+ do{
+    let opcionIngresada = parseInt(prompt(`Ingrese el Nº de la opcion que desée;
+    1- mostrar catálogo de productos
+    2- Eliminar un producto
+    3- mostrar precios de los productos mas Iva
+    0- Salir de la tienda`))
+    switch(opcionIngresada){
+        case 1:
+            console.log(`Nuestro catálogo es; `) 
+            producto1.mostrarProducto()
+            producto2.mostrarProducto()
+            producto3.mostrarProducto()
+            producto4.mostrarProducto()  
+              
+            break
+        case 2:
+            let noElimMasProduc = false
+            do{
+                let eliminarP = parseInt(prompt(`Ingresa que producto deseas eliminar;
+                1- Eliminar el último producto del catálogo 
+                2- Eliminar el primer producto del catálogo
+                3- Eliminar mas de un producto
+                4- Mostrar catálogo original
+                o- No quiero eliminar más productos de la tienda`
+                ))
+                switch(eliminarP){
+                    case 1:
+                         catProductos.shift()
+                         console.log(catProductos)
+                    break
+                    case 2:
+                        catProductos.pop()
+                        console.log(catProductos)
+                    break
+                    case 3:
+                        let cantElim = parseInt(prompt(`Entre los 4 productos de nuestro catálogo, ingresa en numeros del 1 al 4 de la cantidad que deseas eliminar`))
+                        catProductos.splice(1,cantElim)
+                        console.log(catProductos)
+                        break
+                    case 4: 
+                    producto1.mostrarProducto()
+                    producto2.mostrarProducto()
+                    producto3.mostrarProducto()
+                    producto4.mostrarProducto()  
+                        
+                        break
+                    case 0:
+                        noElimMasProduc = true
+                    break
+                    default:
+                        console.log("Opción no válida")
+                    break
+
+                }
+            }while(!noElimMasProduc)
+            
+             break
+        case 3:
+             producto1.precioMasIva(producto1.calcuIva()),
+             producto2.precioMasIva(producto2.calcuIva())
+             producto3.precioMasIva(producto3.calcuIva())
+             producto4.precioMasIva(producto4.calcuIva()) 
+            break
+      
+         case 0:
+           console.log(`Saliste de la tienda, gracias ${nombre} por ver nuestro catálogo`)
+           salirMenu = true
+            break
+        default:
+            console.log("Opción no válida")
+        break
+
+    }
+
+ }while(!salirMenu)
+ }
+ menu()
 
 
-
-
-
-
-// producto1.mostrarPreMasIva(mostrarProducto(),precioMasIva())
-// console.log(producto1.mostrarPreMasIva())
-
-
-
-
-
-
-
-
-//  function menu (){
-//     let salirMenu = false
-
-//  do{
-//     let opcionIngresada = parseInt(prompt(`Ingrese el Nº del producto que desea comprar, si no quiere comprar en este momento, elija la opción 0
-//     1- Collar de plata con perlas $ ${precioCollar},
-//     2- Anillo de plata con piedra rose $ ${precioAnillo},
-//     3- Argollas de plata $ ${precioAros},
-//     4- Pulsera de plata con piedra onix $ ${precioPulsera}
-//     0- No quiero comprar más este momento`))
-//     switch(opcionIngresada){
-//         case 1:
-//             console.log(precioCollar)
-//                 total += precioCollar
-//             break
-//         case 2:
-//              console.log(precioAnillo)
-//                 total += precioAnillo
-//              break
-//         case 3:
-//             console.log(precioAros)
-//             total += precioAros
-//             break
-//         case 4:
-//             console.log(precioPulsera)
-//             total += precioPulsera
-//             break
-//          case 0:
-//            console.log("Salir de la compra")
-//            salirMenu = true
-//             break
-//         default:
-//             console.log("Opción no válida")
-//         break
-
-//     }
-
-//  }while(!salirMenu)
-//  }
-//  menu()
-
-//  alert(`el total de la compra es ${total}`)
-
-
-// alert("Ingrese la opcion del producto que desea llevar, para salir ingrese 0")
-// let seleccionarProductos = Number(prompt( "1-buzo $3000 2-remera $1500 3-jean $5000 4-Zapatillas $6000 "))
-// let seleccionarCantidad;
-// let total = 0;
-
-
-// const cantidad = (cant, precio) => {
-//   return cant * precio
-// }
-
-
-// while (seleccionarProductos != 0) {
-//   switch (seleccionarProductos) {
-//     case 1:
-//       seleccionarCantidad= Number(prompt("el producto seleccionado es buzo, indique la cantidad"))
-//             total += cantidad(seleccionarCantidad, 3000)
-//       break;
-//       case 2:
-//         seleccionarCantidad = Number(prompt("el producto seleccionado es remera, indique la cantidad"))
-//         total += cantidad(seleccionarCantidad, 1500)
-//       break;
-//     case 3:
-//       seleccionarCantidad = Number(prompt("el producto seleccionado es jean, indique la cantidad"))
-//       total += cantidad(seleccionarCantidad, 5000)
-//     break;
-//     case 4:
-//       seleccionarCantidad = Number(prompt("el producto seleccionado es Zapatillas, indique la cantidad"))
-//       total += cantidad(seleccionarCantidad, 6000)
-//     break;
-
-//     default:
-//       break;
-//   }
-//   seleccionarProductos = Number(prompt( "1-buzo $3000 2-remera $1500 3-jean $5000 4-Zapatillas $6000 "))
-// }
-
-// alert("el total de la compra es de: " + total)
-
-
-// const envio = () => {
-//     if (total >= 10000) {
-//       alert("El envio es gratuito")
-//     }else{
-//       total += 1000
-//       alert("el costo de envio es de 1000, el total es: " + total)
-//     }
-// }
-
-// // envio()
-
-// const metodoDePago = () => {
-//   let metodo = prompt("ingrese el metodo de pago, tarjeta o efectivo" )
-//   if (metodo == "tarjeta") {
-//     total *= 1.1
-//     console.log(total);
-//   }else if ( metodo == "efectivo") {
-//     total -= 1000
-//     alert("tenes un descuento de 1000, el total es:" + total)
-//   }
-
-
-// }
-
-// metodoDePago()
-
-const btncart = document.querySelector(`.container-icon`)
-const containercartproducts = document.querySelector(`.container-cart-products`)
-
-btncart.addEventListener(`click`, () => (
-    containercartproducts.classList.toggle(`hidden-cart`)
-))
 
 
 
